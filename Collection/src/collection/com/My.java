704b8e25a5dd888d35133ebde83c0403ab16a898
santhosh-568 <99532706+santhosh-568@users.noreply.github.com>
2023-05-05 15:19:44 +0530

    Add files via upload
    Added Microservices and java hands-on

---
+++ Collection/src/collection/com/My.java
@@ -0,0 +1,18 @@
+package collection.com;
+
+public class My {
+	float s=30000;
+}
+class Developer extends My{
+	float b=2000;
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		My obj=new My();
+		System.out.println("salary is:"+obj.s);
+      
+	}
+
+}
